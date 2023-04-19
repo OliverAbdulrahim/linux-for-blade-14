b62134eddcba887511b5a632ef5b4a2f5805c6a6
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-18 18:22:35 -0600

    Refactor: make file paths more portable

--- scripts/offline-update-generate.sh
+++ scripts/offline-update-generate.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Start of the script
-. ./tools.sh
+repository_root_directory="$(realpath "$(dirname "$0")/..")"
+cd "$repository_root_directory" || exit
 
+. scripts/tools.sh
 display_welcome_message
 echo "
-
 This script downloads and packages all the files you need to patch your
 Blade 14's Linux installation.
 
@@ -16,27 +17,32 @@ in Linux.
 "
 read -rp "Enter the kernel version you want to download (for example, 6.2): " kernel_version
 
-. "./get-kernel.sh" "$kernel_version"
+. scripts/get-kernel.sh
 download_kernel_files "$kernel_version"
 
-. "./get-firmware.sh"
-get_linux_firmware
+. scripts/get-firmware.sh
+get_ath11k_firmware
 
-repo_directory="$base_directory/.."
-archive_directory="$repo_directory/out"
-archive_name="$(basename "$save_directory").tar.gz"
+echo -n "
+Copying files and creating .tar archive... "
 
+archive_directory="$repository_root_directory/out"
 mkdir -p "$archive_directory"
 
-echo "Copying files and creating .tar archive..."
-rsync -av --exclude="$archive_directory" "$repo_directory" "$save_directory"
-tar --exclude-vcs -czvf "$archive_directory/$archive_name" -C "$save_directory" ./
+archive_name="$(basename "$save_directory").tar.gz"
+archive_path="$archive_directory/$archive_name"
+
+cp -r "$repository_root_directory/scripts" "$save_directory"
+cp -r "$repository_root_directory/resources" "$save_directory"
+cp "$repository_root_directory/README.md" "$save_directory"
+tar --exclude-vcs -czf "$archive_path" -C "$save_directory" ./
 
 echo "Done!
 
-You can find the archive in the /out folder of this repository! Copy
-this over to your Linux installation using an external drive.
+You can find the archive at the following path:
+'$archive_path'
 
-Extract it there, and run 'offline-update-apply-blade-14.sh' to
-complete the installation.
+Copy this over to your Linux installation using an external drive.
+
+Extract it there and run 'offline-update-apply.sh' to finish.
 "
