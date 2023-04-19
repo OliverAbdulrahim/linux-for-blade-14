56aa90f6447edfc8596e7b5f8ea2abad5fb77067
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-04-18 18:21:18 -0600

    Refactor: only retrieve necessary files with git clone

--- scripts/get-firmware.sh
+++ scripts/get-firmware.sh
@@ -1,25 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-get_linux_firmware() {
-  firmware_directory="$save_directory/linux-firmware"
-  if [ -d "$firmware_directory" ]; then
-    echo "$firmware_directory already exists!"
-    echo -n "Pulling from git tree... "
-    git -C "$firmware_directory" pull
-  else
-    echo -n "Cloning linux-firmware repository... "
-    git clone --quiet git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git "$firmware_directory"
-    echo "Done!"
-  fi
+get_ath11k_firmware() {
+  echo "
+Getting ath11k wireless firmware now..."
+  firmware_directory="$save_directory/firmware"
+  firmware_repo_url="git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git"
+
+  # Only clone the latest commit and only include the root directory and ath11k/ of the firmware repo
+  # --no-checkout: don't create a local copy of the repository
+  # --depth 1: fetch only the most recent commit (no history beyond that)
+  # --sparse: enable sparse checkout mode
+  # --single-branch --branch main: only fetch the "main" branch
+  git clone \
+    --no-checkout \
+    --depth 1 \
+    --sparse \
+    --single-branch --branch main \
+    "$firmware_repo_url" "$firmware_directory"
+  git -C "$firmware_directory" sparse-checkout add "/ath11k"
+  git -C "$firmware_directory" checkout
+
+  echo -n "Cleaning up... "
+  find "$firmware_directory" -type f | grep -v "ath11k" | xargs rm
+  rm -r "$firmware_directory/.git"
+  echo "Done!"
 }
 
 install_ath11k_firmware() {
   if [ -d "$1" ]; then
-    echo -n "Copying ath11k wireless firmware to your /lib/firmware/ ... "
-    sudo cp -r "$1/ath11k" /lib/firmware/
+    echo -n "Copying ath11k wireless firmware to your '/lib/firmware' ... "
+    sudo cp -r "$1/ath11k" /lib/firmware
     echo "Complete!"
     echo "Please note that a reboot is required for the changes to take effect."
   else
-    echo "Error: could not find linux-firmware in $1"
+    echo "Error: could not find firmware files in $1"
   fi
 }
